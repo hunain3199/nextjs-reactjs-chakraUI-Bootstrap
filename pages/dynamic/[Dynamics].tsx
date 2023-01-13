0da90707e791b3ab6489658b73aaa9bfa2d03317
hunain3199 <muhammadhunain3199@gmail.com>
2023-01-14 03:54:55 +0500

    initial

--- pages/dynamic/[Dynamics].tsx
+++ pages/dynamic/[Dynamics].tsx
@@ -3,11 +3,11 @@ import { Button} from '@chakra-ui/react';
 
 const Dynamic_routes = () => {
     const router = useRouter()
-    const { Dynamic } = router.query
+    const { Dynamics } = router.query
     
     return (<>
-    <h2>This is {Dynamic} page</h2>
-    <h2>Post: {Dynamic}</h2>
+    <h2>This is {Dynamics} page</h2>
+    <h2>Post: {Dynamics}</h2>
 
     <Button colorScheme='blue' variant='solid' onClick={() => router.push('/')}>Go Back To Home Page</Button>
     </>)
